592aacb6523a0ec6b18a46b3fe5448e908eae12e
Vamja Chintan <56104536+chintan033@users.noreply.github.com>
2020-08-13 17:17:56 +0530

    Update 2-fibonacci.py

--- 2-fibonacci.py
+++ 2-fibonacci.py
@@ -1,7 +1,14 @@
-count = 2
+x = 0
+y = 1
+count = 0
 total = 0
+print("Fibonacci Number: ")
 while count < 4000000:
     if count % 2 == 0:
         total += count
-    count += (count - 1)
+    x = y
+    y = count
+    count = (x + y)
+    
+    
 print(count)
