2a60472d81fc07d285f04dc6c6ee9f87e133421c
Vamja Chintan <56104536+chintan033@users.noreply.github.com>
2020-08-14 18:10:22 +0530

    Update 7-prime.py

--- 7-prime.py
+++ 7-prime.py
@@ -1,25 +1,14 @@
-import time
-import math
+limit = 10001
+x = 3
+count = 1
+prime = 2
+while count < limit:
+    for i in range(3, int(x ** 0.5) + 1, 2):
+        if x % i == 0:
+            break
+        else:
+            prime = x
+            count += 1
+        i += 2
 
-start = time.time()
-
-
-def value(n):
-    is_prime = [True]*n
-    is_prime[0] = False
-    is_prime[1] = False
-    for i in range(2, int(math.sqrt(n) + 1)):
-        index = i*2
-        while index < n:
-            is_prime[index] = False
-            index = index + i
-
-    prime = []
-    for i in range(n):
-        if is_prime[i] == True:
-            prime.append(i)
-    return prime
-
-
-nth_prime = 100001
-print(value(nth_prime))
+print(prime)
