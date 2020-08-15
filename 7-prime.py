7b051cd536cdc89dc7532269b551b10749cd2ef2
Vamja Chintan <56104536+chintan033@users.noreply.github.com>
2020-08-15 16:42:58 +0530

    Update 7-prime.py

--- 7-prime.py
+++ 7-prime.py
@@ -1,11 +1,12 @@
 import sympy
+n = int(input("Enter the value: "))
 count = 2
 num = []
 
-while not len(num) == 10001:
+while not len(num) == n:
     if sympy.isprime(count):
         num.append(count)
     count += 1
     
     
-print(f"Prime Number: {num[10000]}")
+print(f"Prime Number: {num[n - 1]}")
